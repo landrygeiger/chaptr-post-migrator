f3c5bf4f4e5946a68b81de6f73c4da33de3441db
john <johnmichaelpost@gmail.com>
2024-03-24 16:00:58 -0500

    add theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import RichTextEditor from "./editor/RichTextEditor";
+import "./global.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <h1>Chaptr Post Migrator</h1>;
+  const [content, setContent] = useState("");
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#79242F",
+      },
+      secondary: {
+        main: "#9e8959",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>Chaptr Post Migrator</h1>
+        <RichTextEditor content={content} setContent={setContent} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
